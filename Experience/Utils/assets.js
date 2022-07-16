f99d51e6c1b44471866ac6ea9f3a9745d4133732
That Random <andrewwoan@gmail.com>
2022-07-16 10:44:15 -0400

    Upgraded to Vite 3.0

---
+++ Experience/Utils/assets.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        name: "pyramid",
+        type: "glbModel",
+        path: "/models/Pyramid.glb",
+    },
+    {
+        name: "pyramidTexture",
+        type: "basisTexture",
+        path: "/textures/PyramidTest.ktx2",
+    },
+];
